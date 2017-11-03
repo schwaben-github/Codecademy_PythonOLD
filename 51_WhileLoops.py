ccf889c3c66d5a0cc7ee947986bc4274ee4378fb
Tibor W <schwaben-github@users.noreply.github.com>
2017-11-03 15:24:21 +0100

    Create 51_WhileLoops.py

---
+++ 51_WhileLoops.py
@@ -0,0 +1,8 @@
+#51. While loops
+#This is not the actual solution!
+#Every try was throwing an error
+#So I copy-pasted the example and was able to continue
+num = 1
+while num > 0:
+    num = raw_input("Number: ")
+    break
