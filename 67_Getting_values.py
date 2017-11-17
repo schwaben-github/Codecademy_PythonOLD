efb5428422452a9001c91b8b545fc52a45d101e7
Tibor W <schwaben-github@users.noreply.github.com>
2017-11-17 13:38:15 +0100

    Create 67_Getting_values.py

---
+++ 67_Getting_values.py
@@ -0,0 +1,5 @@
+#67. Getting all of the values
+pets = {'Boxo': 'dog', 'Niko': 'cat', 'Bluebell': 'cat', 
+        'Gizmo': 'dog', 'Pete': 'parrot', 'Coe': 'betta'}
+for species in pets.values():
+    print(species)
